1b45f68dfd1889db027f6ef3c3e68b4d8b7e29c5
nisarg-ujjainkar <nisarg.ujjainkar@iitgn.ac.in>
2020-07-19 18:48:53 +0530

    added example

--- core/out.s
+++ core/out.s
@@ -1,26 +1,20 @@
  AREA ASDF, CODE, READONLY
 	ENTRY
-	MOV r2, #5
+	MOV r2, #10
 	ADD r10, r2, #0
-	MOV r3, #6
+	MOV r3, #0
 	ADD r11, r3, #0
 while1
 	MOV r3, #0
 	CMP r10,r3
 	BLE end_while1
-ifElse1
-	MOV r3, #0
-	CMP r11,r3
-	BLE else1
-	MOV r3, #1
-	SUB r0, r11, r3
+	MUL r0, r10, r10
+	ADD r12, r0, #0
+	ADD r0, r11, r12
 	ADD r11, r0, #0
-	B else1
-end_ifElse1
 	MOV r2, #1
 	SUB r0, r10, r2
 	ADD r10, r0, #0
-end_ifElse1
 	B while1
 end_while1
 	END
